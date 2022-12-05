da2047d27d7032c0893c14145022708018c57ebd
riku0402 <04022005rikuriku@gmail.com>
2022-12-05 15:06:08 +0900

    ショップ機能追加

--- Assets/Script/Manager/UIManager.cs
+++ Assets/Script/Manager/UIManager.cs
@@ -13,9 +13,24 @@ public class UIManager : SingletonMonoBehaviour<UIManager>
     [Header("コインテキスト")]
     Text _coinText;
 
+    [SerializeField]
+    [Header("回復きのこ")]
+    Button _mushroomButton;
+
     private void Start()
     {
         _expSlider.value = ScoreManager.Instance.Exp;
         _coinText.text = ScoreManager.Instance.AllCoin.ToString();
+        _mushroomButton.onClick.AddListener(() => BuyMushroom(10));
+    }
+
+    void BuyMushroom(int price)
+    {
+        if (ScoreManager.Instance.AllCoin >= price)
+        {
+            print("買えました");
+            ScoreManager.Instance.Decrease(price);
+            _coinText.text = ScoreManager.Instance.AllCoin.ToString();
+        }
     }
 }
